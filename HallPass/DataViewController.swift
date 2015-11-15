a61b3d7065bd279085acc335c2ecbe39e896c3b7
Elias J. Perez <eperez@pinch.me>
2015-11-15 15:12:34 -0500

    Initial Commit

---
+++ HallPass/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  HallPass
+//
+//  Created by Elias Perez on 11/15/15.
+//  Copyright © 2015 Elias Perez. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
